74c29ed2069ae212726266f59f2327a6996b75a7
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2026-09-01 08:56:31 +0530

    model change to qwen3-vl-plus

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_BASE_URL = os.environ.get("API_BASE_URL", "https://dashscope.aliyuncs.com/co
 CAMERA_INDEX = int(os.environ.get("CAMERA_INDEX", "0"))
 POLL_INTERVAL = int(os.environ.get("POLL_INTERVAL", "3"))
 
-MODEL = os.environ.get("VL_MODEL", "qwen-vl-max")
+MODEL = os.environ.get("VL_MODEL", "qwen3-vl-plus-2025-12-19")
 MAX_LONG_SIDE = int(os.environ.get("MAX_LONG_SIDE", "1280"))
 CAMERA_WARMUP_FRAMES = int(os.environ.get("CAMERA_WARMUP_FRAMES", "5"))
 ALERTS_DIR = os.environ.get("ALERTS_DIR", "alerts")
